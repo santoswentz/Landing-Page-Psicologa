de427addbe9a26d1af125ec9ce33edeecca1480a
eduardowentz <134004996+santoswentz@users.noreply.github.com>
2025-10-01 16:56:06 -0300

    Landing-page

---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4d6f85',       // azul acinzentado (principal)
+        secondary: '#cb5226',     // laranja queimado (destaque/CTA)
+        neutralLight: '#f4f2ee',  // fundo claro
+        neutralDark: '#292223',   // texto escuro
+        accent: '#e18b51',        // dourado suave (hover/detalhes)
+        accentLight: '#8ca5b5',   // azul claro (apoio/harmonização)
+      },
+    },
+  },
+};
